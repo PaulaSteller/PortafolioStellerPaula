10a95108929233986704fc41dc81323fb440eb53
PaulaSteller <paula.stelleralvarado2006@gmail.com>
2026-02-17 02:36:42 -0600

    semana 2

--- src/main/java/com/tienda_paula/TiendaPaulaApplication.java
+++ src/main/java/com/tienda_paula/TiendaPaulaApplication.java
@@ -2,13 +2,13 @@ package com.tienda_paula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication // <--- CORREGIDO: Solo se usa la anotación base
-@ComponentScan (basePackages = {"Tienda_Paula", "com.tienda"}) // cambios
+@SpringBootApplication
+
 public class TiendaPaulaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TiendaPaulaApplication.class, args);
     }
 }
+
