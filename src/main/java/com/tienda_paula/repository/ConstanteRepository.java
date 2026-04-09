f64aecdf77f64d29d6a6d73682ba0829c89518ad
PaulaSteller <paula.stelleralvarado2006@gmail.com>
2026-04-08 19:44:46 -0600

    semana 13

---
+++ src/main/java/com/tienda_paula/repository/ConstanteRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda_paula.repository;
+
+/**
+ *
+ * @author paulasteller
+ */
+import com.tienda_paula.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstanteRepository extends JpaRepository<Constante,Integer> {
+    
+    public Optional<Constante> findByAtributo(String atributo);
+    
+}
